671a09a557d93e97f8ea93b3fad66cd18d3c08ca
Dhanvi <112699286+itz-jerry3@users.noreply.github.com>
2023-10-25 23:17:13 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+/*For Typing Effect*/
+
+var typed = new Typed(".typing",{
+    strings:["","Student..","Web Designer..","","Web Developer.."],
+    typeSpeed:100,
+    BackSpeed:50,
+    loop:true
+
+})
